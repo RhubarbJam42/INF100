62f67de0dad51ef0faa0a2517ec3b4ab270ba07a
RhubarbJam42 <90278809+RhubarbJam42@users.noreply.github.com>
2021-09-13 11:43:25 +0200

    fullført oppgave 11

---
+++ uke4Oppgaver/uke_04_oppg_11.py
@@ -0,0 +1,15 @@
+#programmet beregner hvor lang tid det tar en stein å nå bakken fra en gitt høydeposisjon
+startPosition = float(input('Stenen droppes fra høyde: '))
+time = float(0)
+gravityConstant = float(startPosition - 1.0 / 2.0 * 9.8 * time ** 2.0)
+position = (startPosition - 1.0 / 2.0 * 9.8 * time ** 2.0)
+landed = 0
+while position > 0:
+    position = round(position, 2)
+    print(f'{position} m')
+    time = time + 1.0
+    position = (startPosition - 1.0 / 2.0 * 9.8 * time ** 2.0)
+time = int(time)
+print(f'{landed} m')
+betweenTime = int(time - 1)
+print(f'Stenen lander mellom {betweenTime} og {time} sekunder etter at den droppes.')
