c8fca7a4e172ee2fb94452a2dc5a20ef3903d64e
RhubarbJam42 <90278809+RhubarbJam42@users.noreply.github.com>
2021-10-06 14:06:43 +0200

    Uke 7 oppgave 2 rettet

--- uke7Oppgaver/uke_07_oppg_2.py
+++ uke7Oppgaver/uke_07_oppg_2.py
@@ -1,18 +1,19 @@
-#funksjon som tar en streng med én ukes temperaturer og gjør de om til float
+# funksjon som tar en streng med én ukes temperaturer og gjør de om til float
 
 def update_weather(temperatures):
-    #lager en ny liste som legger til float verdier i listen. Returnerer tuple av listen
+    # lager en ny liste som legger til float verdier i listen. Returnerer tuple av listen
     list_of_temperatures = temperatures.split(' ')
     updated_temperature = []
     for item in list_of_temperatures:
         updated_temperature.append(float(item))
     return tuple(updated_temperature)
 
-def tuesday_weather(temperatures):
-    #splitter strengen om til en liste og returnerer index 1 som er "Tirsdag" 
-    list_of_temperatures = temperatures.split(' ')
-    return list_of_temperatures[1]
 
+def tuesday_weather(temperatures):
+    #tar inn tuple og gir tilbake index 1
+    return float(temperatures[1])
+
+#tester
 #print(update_weather("16.1 14.1 13.3 15.0 13.0 13.2 12.9"))
 #print(tuesday_weather("16.1 14.1 13.3 15.0 13.0 13.2 12.9"))
-
+#print(tuesday_weather((16.1, 14.1, 13.3, 15.0, 13.0, 13.2, 12.9)))
