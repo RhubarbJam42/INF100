ef84c9882050f5d0d6033768ad3c4d3a2c05ae6e
RhubarbJam42 <90278809+RhubarbJam42@users.noreply.github.com>
2021-10-07 12:03:04 +0200

    Oppgave 6 gjort

--- uke7Oppgaver/uke_07_oppg_6.py
+++ uke7Oppgaver/uke_07_oppg_6.py
@@ -1,1 +1,36 @@
-#pigify funksjon
+#pigify funksjon: Oversetter engelsk til "pig latin"
+from copy import copy
+def pigify(word):
+    vowels = ('a', 'e', 'i', 'o', 'u')
+    pig_word = ''
+    #dersom ordet starter med vokal
+    if word[0] in vowels:
+        pig_word = copy(word) + 'way'
+    #dersom ordet ikke starter med vokal
+    else:
+        pig_word = list(word)[give_index(word):] + list(word)[:give_index(word)]
+        pig_word += 'ay'
+    return ''.join(pig_word)
+
+
+def give_index(word):
+    vowels = ('a', 'e', 'i', 'o', 'u')
+    for index, letter in enumerate(word):
+        if letter in vowels:
+            return index
+    return -1
+
+
+def pigify_sentence(sentence):
+    pig_sentence = []
+    sentence_list = sentence.split()
+    for word in sentence_list:
+        pig_sentence.append(pigify(word))
+    return ' '.join(pig_sentence)
+
+
+
+#test
+#print(pigify('oolong'))
+#print(pigify('floor'))
+#print(pigify_sentence('alice was beginning to get very tired'))
