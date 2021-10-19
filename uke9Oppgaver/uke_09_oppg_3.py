845b9e0a9be05070b5d3e7df2ec7ca2bacc673c1
RhubarbJam42 <90278809+RhubarbJam42@users.noreply.github.com>
2021-10-19 11:52:43 +0200

    Oppgaver uke 9 (oppg 1 til og med 6 gjort)

---
+++ uke9Oppgaver/uke_09_oppg_3.py
@@ -0,0 +1,32 @@
+# skrive ut keys og values til en dictionary i tilleg til i en liste
+
+myDictionary = {0: 0, 1: 'vafler', 'two': 2, 5: 4}
+myList = [0, 1, 'boller', 4]
+
+print('Dictionary Keys:')
+for key in myDictionary.keys():
+    print(key)
+
+print('')
+
+print('Dictionary Values:')
+for value in myDictionary.values():
+    print(value)
+
+print('')
+
+print('Dictionary keys/value:')
+for key, itemValue in myDictionary.items():
+    print(key, itemValue)
+
+print('')
+
+print('List values:')
+for items in myList:
+    print(items)
+
+print('')
+
+print('List indices/values:')
+for i, v in enumerate(myList):
+    print(i, v)
